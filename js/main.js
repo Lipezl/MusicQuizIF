fbb7fe07478b3f2ddec89e91fee3930d0c724548
Felipe Silva <115328437+Lipezl@users.noreply.github.com>
2024-10-04 20:29:32 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -118,72 +118,72 @@ function mostrarMusicasPop() {
             correta: 'Taylor Swift'
         },
         {
-            musica: '../musicas/Pop/babyJustinBieber.mp3',
+            musica: 'musicas/Pop/babyJustinBieber.mp3',
             alternativas: ['Drake', 'Justin Bieber', 'Ed Sheeran', 'Wiz Khalifa'],
             correta: 'Justin Bieber'
         },
         {
-            musica: '../musicas/Pop/roarKatyPerry.mp3',
+            musica: 'musicas/Pop/roarKatyPerry.mp3',
             alternativas: ['Katy Perry', 'Adele', 'Lady Gaga', 'Beyoncé'],
             correta: 'Katy Perry'
         },
         {
-            musica: '../musicas/Pop/diamondsRihanna.mp3',
+            musica: 'musicas/Pop/diamondsRihanna.mp3',
             alternativas:  ['Rihanna', 'Ariana Grande', 'Selena Gomez', 'Taylor Swift'],
             correta: 'Rihanna'
         },
         {
-            musica: '../musicas/Pop/uptownFunk.mp3',
+            musica: 'musicas/Pop/uptownFunk.mp3',
             alternativas:  ['Ed Sheeran', 'Justin Bieber', 'Bruno Mars', 'The Weeknd'],
             correta:  'Bruno Mars'
         },
         {
-            musica: '../musicas/Pop/sugarMaroon5.mp3',
+            musica: 'musicas/Pop/sugarMaroon5.mp3',
             alternativas: ['Akon', 'Bruno Mars', 'Ed Sheeran', 'Maroon 5'],
             correta: 'Maroon 5'
         },
         {
-            musica: '../musicas/Pop/congratulationsPostMalone.mp3',
+            musica: 'musicas/Pop/congratulationsPostMalone.mp3',
             alternativas:  ['Justin Bieber', 'Post Malone', 'Drake', 'Ariana Grande'],
             correta: 'Post Malone'
         },
         {
-            musica: '../musicas/Pop/sideToSideAriana.mp3',
+            musica: 'musicas/Pop/sideToSideAriana.mp3',
             alternativas: ['Cardi B', 'Katy Perry', 'Ariana Grande', 'Rihanna'],
             correta: 'Ariana Grande'
         },
         {
-            musica: '../musicas/Pop/countingStarsOneRepublic.mp3',
+            musica: 'musicas/Pop/countingStarsOneRepublic.mp3',
             alternativas: ['OneRepublic', 'The Weeknd', 'Ed Sheeran', 'One Direction'],
             correta: 'OneRepublic'
         },
         {
-            musica: '../musicas/Pop/wakaWakaShakira.mp3',
+            musica: 'musicas/Pop/wakaWakaShakira.mp3',
             alternativas: ['Jennifer Lopez', 'Shakira', 'Beyonce', 'Rihanna'],
             correta: 'Shakira'
         },
         {
-            musica: '../musicas/Pop/iGottaFeeling.mp3',
+            musica: 'musicas/Pop/iGottaFeeling.mp3',
             alternativas: ['David Guetta', 'LMFAO', 'Flo Rida', 'Black Eyed Peas'],
             correta: 'Black Eyed Peas'
         },
         {
-            musica: '../musicas/Pop/oneKissDuaLipa.mp3',
+            musica: 'musicas/Pop/oneKissDuaLipa.mp3',
             alternativas: ['Calvin Harris', 'Dua Lipa', 'Ellie Goulding', 'Rita Ora'],
             correta: 'Dua Lipa'
         },
         {
-            musica: '../musicas/Pop/adventureColdplay.mp3',
+            musica: 'musicas/Pop/adventureColdplay.mp3',
             alternativas: ['Maroon 5', 'MAGIC!', 'Coldplay', 'Imagine Dragons'],
             correta: 'Coldplay'
         },
         {
-            musica: '../musicas/Pop/justGivePink.mp3',
+            musica: 'musicas/Pop/justGivePink.mp3',
             alternativas: ['Kelly Clarkson', 'Alicia Keys', 'Pink', 'Lady Gaga'],
             correta: 'Pink'
         },
         {
-            musica: '../musicas/Pop/theHills.mp3',
+            musica: 'musicas/Pop/theHills.mp3',
             alternativas: ['The Weeknd', 'Drake', 'Khalid', 'Post Malone'],
             correta: 'The Weeknd'
         }
@@ -195,77 +195,77 @@ function mostrarMusicasPop() {
 function mostrarMusicasTrap() {
     let musicas_rap = () => [
         {
-            musica: '../musicas/Rap/godsplanDrake.mp3',
+            musica: 'musicas/Rap/godsplanDrake.mp3',
             alternativas: ['Kanye West', 'Kendrick Lamar', 'Drake', 'Travis Scott'],
             correta: 'Drake'
         },
         {
-            musica: '../musicas/Rap/goosebumpsTravisScostt.mp3',
+            musica: 'musicas/Rap/goosebumpsTravisScostt.mp3',
             alternativas: ['Travis Scott','Playboi Carti','21 Savage', 'Future'],
             correta:'Travis Scott'
         },
         {
-            musica: '../musicas/Rap/heartlessKanyeWest.mp3',
+            musica: 'musicas/Rap/heartlessKanyeWest.mp3',
             alternativas:['The Weekend','Frank Ocean','Kanye West','Ty Dolla Sign'],
             correta: 'Kanye West'
         },
         {
-            musica: '../musicas/Rap/inDaClub50Cent.mp3',
+            musica: 'musicas/Rap/inDaClub50Cent.mp3',
             alternativas: ['Dr. Dre', '50 Cent', 'Pop Smoke', 'Notorious BIG'],
             correta: '50 Cent'
         },
         {
-            musica: '../musicas/Rap/loveYourselfEminem.mp3',
+            musica: 'musicas/Rap/loveYourselfEminem.mp3',
             alternativas:  ['Eminem', 'Kanye West', 'Drake', 'Travis Scott'],
             correta: 'Eminem'
         },
         {
-            musica: '../musicas/Rap/humbleKendrick.mp3',
+            musica: 'musicas/Rap/humbleKendrick.mp3',
             alternativas: ['50 Cent', 'Kendrick Lamar', 'Jay Z', 'Drake'],
             correta: 'Kendrick Lamar'
         },
         {
-            musica: '../musicas/Rap/rockstarPostMalone.mp3',
+            musica: 'musicas/Rap/rockstarPostMalone.mp3',
             alternativas: ['Roddy Ricch', 'Lil Baby', 'Lil Wayne',  'Post Malone'],
             correta: 'Post Malone'
         },
         {
-            musica: '../musicas/Rap/crankThatSouljaBoy.mp3',
+            musica: 'musicas/Rap/crankThatSouljaBoy.mp3',
             alternativas: ['Soulja Boy', 'Lil Wayne', '50 Cent', 'Eminem'],
             correta: 'Soulja Boy'
         },
         {
-            musica: '../musicas/Rap/theBoxRoddy.mp3',
+            musica: 'musicas/Rap/theBoxRoddy.mp3',
             alternativas: ['Gunna','Roddy Ricch', 'Lil Baby', 'Lil Wayne'],
             correta: 'Roddy Ricch'
         },
         {
-            musica: '../musicas/Rap/ransomLilTecca.mp3',
+            musica: 'musicas/Rap/ransomLilTecca.mp3',
             alternativas:  ['Lil Tecca', 'Lil Baby', 'Lil Wayne', 'Lil Durk'],
             correta: 'Lil Tecca'
         },
         {
-            musica: '../musicas/Rap/allMyLife.mp3',
+            musica: 'musicas/Rap/allMyLife.mp3',
             alternativas: ['J. Cole', 'Lil Durk', 'Lil Uzi Vert', 'Young Thug'],
             correta: 'Lil Durk'
         },
         {
-            musica: '../musicas/Rap/noRuleJCole.mp3',
+            musica: 'musicas/Rap/noRuleJCole.mp3',
             alternativas: ['Drake', 'Travis Scott', 'Lil Wayne', 'J. Cole'],
             correta: 'J. Cole'
         },
         {
-            musica: '../musicas/Rap/maskOff.mp3',
+            musica: 'musicas/Rap/maskOff.mp3',
             alternativas: ['Gunna', '21 Savage', 'Future', 'A$AP ROCKY'],
             correta: 'Future'
         },
         {
-            musica: '../musicas/Rap/bankAccount21.mp3',
+            musica: 'musicas/Rap/bankAccount21.mp3',
             alternativas: ['21 Savage', 'Future', 'Gunna', 'Lil Baby'],
             correta: '21 Savage'
         },
         {
-            musica: '../musicas/Rap/shootaCarti.mp3',
+            musica: 'musicas/Rap/shootaCarti.mp3',
             alternativas: ['Playboi Carti', 'Trippie Red', 'NLE Choppa', 'Roddy Ricch'],
             correta:  'Playboi Carti'
         }
@@ -278,32 +278,32 @@ function mostrarMusicasTrap() {
 function mostrarMusicaRock (){
     let musicas_rock = ()=>[
         {
-            musica:  '../musicas/Rock/comeAsYouAre.mp3',
+            musica:  'musicas/Rock/comeAsYouAre.mp3',
             alternativas: ['Red', 'Pearl Jam', 'Foo Fighters', 'Nirvana'],
             correta: 'Nirvana'
         },
         {
-            musica: '../musicas/Rock/welcomeToTheJungle.mp3',
+            musica: 'musicas/Rock/welcomeToTheJungle.mp3',
             alternativas: ['AC/DC', "Guns N' Roses", 'Queen', 'Led Ze'],
             correta: "Guns N' Roses"
         },
         {
-            musica: '../musicas/Rock/demonsImagineDragons.mp3',
+            musica: 'musicas/Rock/demonsImagineDragons.mp3',
             alternativas: ['Coldplay', 'Twenty One Pilots', 'Imagine Dragons', 'The Weeknd'],
             correta: 'Imagine Dragons'
         },
         {
-            musica: '../musicas/Rock/weWillRockYou.mp3',
+            musica: 'musicas/Rock/weWillRockYou.mp3',
             alternativas: ['Queen',  'AC/DC', "Guns N' Roses", 'Nirvana'],
             correta:  'Queen'
         },
         {
-            musica: '../musicas/Rock/californication.mp3',
+            musica: 'musicas/Rock/californication.mp3',
             alternativas:  ['Pearl Jam', 'Foo Fighters', 'Nirvana', 'Red Hot Chili Peppers'],
             correta: 'Red Hot Chili Peppers'
         },
         {
-            musica: '../musicas/Rock/numbLinkinPark.mp3',
+            musica: 'musicas/Rock/numbLinkinPark.mp3',
             alternativas: ['Twenty One Pilots',  'Linkin Park', 'The Weeknd', 'Imagine Dragons'],
             correta: 'Linkin Park'
         }
